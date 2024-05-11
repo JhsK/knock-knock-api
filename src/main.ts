dd0cb528973ebf4226e8433b4faf471adc640eaa
JhsK <qwe6293@nate.com>
2024-05-11 19:51:03 +0900

    feat: 환경변수 설정 및 PORT 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  console.log(`PORT ${port} - 서버 실행중입니다.`);
+  await app.listen(port);
 }
 bootstrap();
