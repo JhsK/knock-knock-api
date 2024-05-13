e0e787e345f48a35ae7d598577c0c6662dfd801c
JhsK <qwe6293@nate.com>
2024-05-13 22:44:23 +0900

    feat: typeorm, mysql 설치 및 typeorm config 작성

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: 'knock-knock',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
