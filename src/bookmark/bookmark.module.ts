773bdb2d2730ac99941a45117e65a4ef25b0e18a
Jhsk <qwe6293@nate.com>
2024-05-17 17:39:52 +0900

    feat: bookmark 엔티티 모듈에 추가

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BookmarkController } from './bookmark.controller';
 import { BookmarkService } from './bookmark.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bookmark } from './bookmark.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Bookmark])],
   controllers: [BookmarkController],
-  providers: [BookmarkService]
+  providers: [BookmarkService],
 })
 export class BookmarkModule {}
