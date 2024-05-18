ae9178565ea10df4c378513e3507f943664a9099
JhsK <qwe6293@nate.com>
2024-05-18 14:04:59 +0900

    feat: kakao 토큰 및 유저 정보 response dto 정의

--- src/user/dto/oauth/user.response.ts
+++ src/user/dto/oauth/user.response.ts
@@ -2,7 +2,7 @@ import { SocialEnum } from 'src/user/types/user';
 
 export interface OAuthUserResponse {
   socialId: number;
-  registerdAt: string;
+  registeredAt: string;
   nickname: string;
   email?: string;
   type: keyof typeof SocialEnum;
