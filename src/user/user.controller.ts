b08fe38c5d37104bb42db3a38a833b252eae25d4
JhsK <qwe6293@nate.com>
2024-05-18 14:47:20 +0900

    feat: request에 user 객체를 바로 받을 수 있는 커스텀 데코레이터 작성

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
 import {
   Body,
   Controller,
+  Get,
+  Param,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { OAuthService } from './services/oauth.service';
 import { KakaoLoginRequest } from './dto/kakao/kakao-login.request';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
 export class UserController {
@@ -22,5 +26,11 @@ export class UserController {
     const tokens = await this.oauthService.createKakaoUser(body);
 
     return tokens;
+  }
+
+  @Get(':id')
+  @UseGuards(AuthGuard())
+  async getUserById(@Param('id') id: string) {
+    console.log(id);
   }
 }
