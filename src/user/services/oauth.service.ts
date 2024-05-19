5951969a9e13bab4f537d7f23627ba47712abd51
JhsK <qwe6293@nate.com>
2024-05-19 17:45:29 +0900

    feat: 네이버 로그인 회원 생성 및 동시성 이슈 트랜젝션 처리

--- src/user/services/oauth.service.ts
+++ src/user/services/oauth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { KakaoLoginRequest } from '../dto/kakao/kakao-login.request';
+import { NaverLoginRequest } from '../dto/naver/naver-login.request';
 import { OAuthFactory } from '../oauth.factory';
 import { SocialEnum } from '../types/user';
 import { User } from '../user.entity';
-import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class OAuthService {
@@ -15,6 +16,7 @@ export class OAuthService {
 
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async createKakaoUser(kakaoLoginRequest: KakaoLoginRequest) {
@@ -64,5 +66,53 @@ export class OAuthService {
       accessToken,
       refreshToken,
     };
+  }
+
+  async createNaverUser(naverLoginRequest: NaverLoginRequest) {
+    const naverService = await this.oauthFactory.createOAuthService('naver');
+    const naverUser = await naverService.getUser(naverLoginRequest.accessToken);
+
+    if (!naverUser) {
+      throw new NotFoundException();
+    }
+
+    const { nickname, socialId, registeredAt, type } = naverUser;
+    const socialType = SocialEnum[type];
+
+    return this.dataSource.transaction(async (manager) => {
+      const userRepository = manager.getRepository(User);
+      const user = await userRepository.findOne({
+        where: { socialId: socialId as string, socialType },
+      });
+
+      if (user) {
+        const payload = { userId: user.id, socialType };
+        const accessToken = await this.jwtService.signAsync(payload);
+        const refreshToken = await this.jwtService.signAsync(payload, {
+          expiresIn: '7d',
+        });
+
+        return { accessToken, refreshToken };
+      }
+      const createUser = await userRepository.create({
+        nickname,
+        socialId: socialId as string,
+        socialType,
+        registerAt: registeredAt,
+      });
+
+      await userRepository.save(createUser);
+
+      const payload = { userId: createUser.id, socialType };
+      const accessToken = await this.jwtService.signAsync(payload);
+      const refreshToken = await this.jwtService.signAsync(payload, {
+        expiresIn: '7d',
+      });
+
+      return {
+        accessToken,
+        refreshToken,
+      };
+    });
   }
 }
