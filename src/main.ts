79aea662851bbac1c085dc0c0656618264fa2b32
JhsK <qwe6293@nate.com>
2024-08-17 14:51:02 +0900

    feat: cookie-parser 설치 및 설정

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
   const logger = new Logger();
 
   app.enableCors();
+  app.use(cookieParser());
 
   await app.listen(port);
   logger.log(`Application running on port ${port}`);
